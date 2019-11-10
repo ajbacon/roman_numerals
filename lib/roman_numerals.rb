a7214bd4077abcb63b0b326df9a6a9090c580203
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-10 11:47:01 +0000

    added tests for more complex number 6,7&8

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,8 +1,32 @@
 class RomanNumerals
 
-  def self.convert(num)
-    return "V" if num == 5
-    "I"
-  end
+  NUMERALS = {
+    1000 => "M",
+    500 => "D",
+    100 => "C",
+    50 => "L",
+    10 => "X",
+    5 => "V",
+    1 => "I"
+  }
 
+  NUMERALS2 = {
+    5 => "V",
+    1 => "I"
+  }
+  def self.convert(num)
+    numeral = ""
+    
+    NUMERALS2.each do |digit, letter| 
+      (num/digit).times do 
+        numeral << letter
+        num -= digit 
+      end 
+    end
+    
+    numeral
+    
+  end
 end
+
+
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -7,4 +7,40 @@ describe RomanNumerals do
   it "must be able to convert 5 to 'V'" do
     expect(RomanNumerals.convert(5)).to eq("V")
   end
+
+  # it "must be able to convert 10 to 'X'" do
+  #   expect(RomanNumerals.convert(10)).to eq("X")
+  # end
+
+  # it "must be able to convert 50 to 'L'" do
+  #   expect(RomanNumerals.convert(50)).to eq("L")
+  # end
+
+  # it "must be able to convert 100 to 'C'" do
+  #   expect(RomanNumerals.convert(100)).to eq("C")
+  # end
+
+  # it "must be able to convert 500 to 'D'" do
+  #   expect(RomanNumerals.convert(500)).to eq("D")
+  # end
+
+  # it "must be able to convert 1000 to 'M'" do
+  #   expect(RomanNumerals.convert(1000)).to eq("M")
+  # end
+
+  #-----
+
+  it "must be able to convert 6 to 'VI'" do
+    expect(RomanNumerals.convert(6)).to eq("VI")
+  end
+
+  it "must be able to convert 7 to 'VII'" do
+    expect(RomanNumerals.convert(7)).to eq("VII")
+  end
+
+  it "must be able to convert 8 to 'VIII'" do
+    expect(RomanNumerals.convert(8)).to eq("VIII")
+  end
+
+
 end
