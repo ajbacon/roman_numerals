f42c87ad39d0fd46c3e7c60f121cfce8169a8d9d
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-10 07:45:55 +0000

    passed first test

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,7 @@
+class RomanNumerals
+
+  def self.convert(num)
+    "I"
+  end
+  
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,3 +1,4 @@
+require 'roman_numerals'
 describe RomanNumerals do
   it "must be able to convert 1 to 'I'" do
     expect(RomanNumerals.convert(1)).to eq("I")
